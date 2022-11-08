520bc951e6b3edac0775da23a37cc15d4aea552f
Mos Qobadi <mos.qobadi@gmail.com>
2022-11-08 16:38:43 +0330

    first commit

--- main.js
+++ main.js
@@ -1,69 +1,136 @@
-const todoAPI = new EasyReq();
-
+const todoForm = document.querySelector(".form");
+const todoInput = document.getElementById("todo-input");
 const todoList = document.querySelector(".todo-list");
-const todoInput = document.querySelector(".todo-input");
 
-document.addEventListener("DOMContentLoaded", getTodos);
+document.addEventListener("DOMContentLoaded", addFromServer);
 
-function getTodos() {
-  todoAPI
-    .get("todoslist.json")
-    .then((data) => addTodosToDOM(data))
-    .catch((err) => console.log(err));
+function addFromServer() {
+  fetch("http://localhost:8080/todos")
+    .then((res) => res.json())
+    .then((data) => showToDOm(data))
+    .catch((err) => reject(err));
 }
 
-function addTodosToDOM(todos) {
+function showToDOm(todos) {
   todos.forEach((todo) => {
     const li = document.createElement("li");
-    li.className = "todo-item";
 
-    li.appendChild(document.createTextNode(todo.todo));
+    li.className = "todo-list-item";
 
     const link = document.createElement("a");
+
+    link.appendChild(document.createTextNode("X"));
+
     link.className = "remove";
+
     li.appendChild(link);
 
+    li.appendChild(
+      document
+        .createElement("div")
+        .appendChild(document.createTextNode(todo.text))
+    );
     todoList.appendChild(li);
   });
 }
 
-function createTodo(todoText) {
-  todoAPI.get("todoslist.json").then((todos) => todos.push(todoText));
-}
+todoForm.addEventListener("submit", addDomElement);
 
-document.querySelector(".btn").addEventListener("click", postTodo);
+function addDomElement(e) {
+  if (todoInput.value !== "") {
+    const todo = todoInput.value;
 
-const todoText = todoInput.value;
+    const li = document.createElement("li");
 
-createTodo(todoText);
+    li.className = "todo-list-item";
 
-function postTodo(e) {
-  todoAPI
-    .post("todoslist.json", todoText)
-    .then((todos) => addTodosToDOM(todos));
+    const link = document.createElement("a");
+
+    link.appendChild(document.createTextNode("X"));
+
+    link.className = "remove";
+
+    li.appendChild(link);
+
+    li.appendChild(
+      document.createElement("div").appendChild(document.createTextNode(todo))
+    );
+    todoList.appendChild(li);
+
+    todoInput.value = "";
+
+    addToServer(todo);
+  } else {
+    showAlert("Please fill the form", "error");
+
+    setTimeout(() => {
+      document.querySelector(".alert").remove();
+    }, 1500);
+  }
 
   e.preventDefault();
 }
 
-// function postTodo(e) {
-//   todoAPI
-//     .post("todoslist.json")
-//     .then((todo) => addTodoToAPI(todo))
-//     .catch((err) => console.log(err));
+function addToServer(todoText) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-//   e.preventDefault();
-// }
+  // const todoText = document.getElementById("todo-input").value;
 
-// function createTodo(todo) {
-//   todoAPI.get("todoslist.json").then((todos) =>
-//     forEach((todo) => {
-//       todo.title = todoInput;
-//     })(todos).push(todo)
-//   );
-// }
+  const raw = JSON.stringify({
+    id: "6",
+    text: todoText,
+    done: false,
+  });
 
-// function addTodoToAPI(todo) {
-//   get("todolist.json")
-//     .then((todos) => todos.push(todo))
-//     .catch((err) => console.log(err));
-// }
+  const todoData = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:8080/todo", todoData)
+    .then((res) => res.json())
+    .then((data) => postToServer(data))
+    .catch((err) => console.log(err));
+}
+
+document
+  .querySelector(".todo-list")
+  .addEventListener("click", removeDomElement);
+
+function removeDomElement(e) {
+  if (e.target.className === "remove") {
+    e.target.parentElement.remove();
+  }
+
+  removeFromServer(e.target.parentElement.textContent);
+
+  e.preventDefault();
+}
+
+function removeFromServer() {
+  const requestOptions = {
+    method: "DELETE",
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:8080/todo/4", requestOptions)
+    .then((response) => response.text())
+    .then((result) => removeFromDOM(result))
+    .catch((error) => console.log("error", error));
+}
+
+function removeFromDOM() {}
+
+function showAlert(message, className) {
+  const container = document.querySelector(".container");
+  const errDiv = document.createElement("div");
+
+  errDiv.appendChild(document.createTextNode(message));
+
+  errDiv.className = `alert ${className}`;
+
+  container.insertBefore(errDiv, todoForm);
+}
